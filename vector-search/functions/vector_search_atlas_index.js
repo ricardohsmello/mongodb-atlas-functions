8c3d74300c777bdb0fea1408b7c694742920b4ac
Ricardo <ricardohsmello@gmail.com>
2024-02-23 05:29:18 -0300

    refactor

---
+++ vector-search/functions/vector_search_atlas_index.js
@@ -0,0 +1,12 @@
+{
+  "mappings": {
+    "dynamic": true,
+    "fields": {
+      "proverb_embedding": {
+        "dimensions": 384,
+        "similarity": "euclidean",
+        "type": "knnVector"
+      }
+    }
+  }
+}
